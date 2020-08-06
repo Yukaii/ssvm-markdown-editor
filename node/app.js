c0fc2d3b01a831f6cde262013917920530c0fc29
Yukai Huang <yukaihuangtw@gmail.com>
2020-08-06 13:20:44 +0000

    Encode base64

--- node/app.js
+++ node/app.js
@@ -1,18 +1,16 @@
 const { render } = require('../pkg/ssvm_nodejs_starter_lib.js');
 
 const http = require('http');
-const url = require('url');
 const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
-  const queryObject = url.parse(req.url,true).query;
-  console.log(queryObject)
-  if (!queryObject['md']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?md=#Hello%20World \n`);
+  const base64Regex = /\/((?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?)\/?/
+  const m = req.url.match(base64Regex)
+  if (!!m) {
+    res.end(render(Buffer.from(m[1], 'base64').toString()))
   } else {
-    res.end(render(queryObject['md']) + '\n');
+    res.writeHead(404, 'Not Found')
   }
 });
 
